b7defc17bc2a54e41ed5323061f627199e810d3e
Jordan Tran <thai.jordan@gmail.com>
2023-09-24 14:34:10 -0700

    fixed font sizing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,11 @@ export default async function Home() {
   const isAuth = !!userId
 
   return (
-    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to teal-100">
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
-            <h1 className="mr-3 text-5x1 font-semibold">resume chat</h1>
+            <h1 className="mr-3 text-5xl font-semibold">ai chat</h1>
             <UserButton afterSignOutUrl="/" />
           </div>
 
